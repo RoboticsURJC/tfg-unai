2524a86c2766b9eaa8a139c02900e5a7e570a10b
Unai Sanz <u.sanz.2019@alumnos.urjc.es>
2023-10-15 22:54:16 +0200

    New RViz config for each robot and instrutions in the network config script added. Layout instructions and commands corrected.

--- zenoh_nodes/config_network.sh
+++ zenoh_nodes/config_network.sh
@@ -13,4 +13,10 @@ sudo ifconfig lo multicast
 
 #Print the interface configuration to make sure everything is right:
 ifconfig
-echo RMW_IMPLEMENTATION=$RMW_IMPLEMENTATION
+echo -e "RMW_IMPLEMENTATION=\033[0;31m$RMW_IMPLEMENTATION\033[0m"
+
+echo -e "To start the simulation execute the following command: \033[1;36mROS_LOCALHOST_ONLY=1 ros2 launch nav2_bringup_multirobot multi_tb3_simulation_launch.py autostart:=True use_sim_time:=True use_composition:=False verbose:=True\033[0m"
+
+echo -e "\033[1;33mCOMMON ERRORS:\033[0m"
+echo -e " - If Gazebo or RViz2 freeze, give any error or they don't work as expected try to kill the processes (Ctrl+C) and run it again."
+echo -e ' - If Gazebo gives an error like "\033[0;31maddress already in use\033[0m" when running it again execute "\033[1;33mkillall gzserver\033[0m" before running it again.'
